657c7f863f873672f355483d2de2969a1c958999
Tom Brodhurst-Hill <tom@Mac-60f81dcce59e.local>
2015-02-01 17:33:31 +1100

    Initial Commit

---
+++ BFWDrawView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFWDrawView
+//
+//  Created by Tom Brodhurst-Hill on 1/02/2015.
+//  Copyright (c) 2015 BareFeetWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BFWDrawView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BFWDrawView
+//
+//  Created by Tom Brodhurst-Hill on 1/02/2015.
+//  Copyright (c) 2015 BareFeetWare. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
