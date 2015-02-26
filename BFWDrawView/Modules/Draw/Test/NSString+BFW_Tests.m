352180705b7bba14bcbb84a4ee9231e61e117143
Tom Brodhurst-Hill <Tom.Brodhurst-Hill@cba.com.au>
2015-02-26 14:15:00 +1100

    CBAAndroidViewController: export Android colors.xml

---
+++ BFWDrawView/Modules/Draw/Test/NSString+BFW_Tests.m
@@ -0,0 +1,59 @@
+//
+//  NSString+BFW_Tests.m
+//  Prototype
+//
+//  Created by Tom Brodhurst-Hill on 26/02/2015.
+//  Copyright (c) 2015 CommBank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "NSString+BFW.h"
+
+@interface NSString_BFW_Tests : XCTestCase
+
+@end
+
+@implementation NSString_BFW_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testPlist {
+    NSString *className = NSStringFromClass([self class]);
+    className = [className stringByReplacingOccurrencesOfString:@"+" withString:@"_"];
+    NSString *plistPath = [[NSBundle bundleForClass:[self class]] pathForResource:className ofType:@"plist"];
+    NSDictionary *plistDict = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+    NSDictionary *selectorExpectationDict = plistDict[@"selectorExpectations"];
+    for (NSString *selectorName in selectorExpectationDict) {
+        NSArray *expectations = selectorExpectationDict[selectorName];
+        for (NSDictionary *expectation in expectations) {
+            id selfValue = expectation[@"self"];
+            SEL selector = NSSelectorFromString(selectorName);
+            BOOL responds = [selfValue respondsToSelector:selector];
+            XCTAssert(responds, @"%@ does not respond to %@", selfValue, selectorName);
+            if (responds) {
+                NSMethodSignature *methodSignature = [selfValue methodSignatureForSelector:selector];
+                NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
+                [invocation setSelector:selector];
+                [invocation setTarget:selfValue];
+                [invocation invoke];
+                id __unsafe_unretained tempReturnValue;
+                [invocation getReturnValue:&tempReturnValue];
+                id testReturnValue = tempReturnValue;
+                id expectReturnValue = expectation[@"return"];
+                NSLog(@"testing [%@ %@] isEqualTo: %@", selfValue, selectorName, expectReturnValue);
+                XCTAssertEqualObjects(expectReturnValue, testReturnValue, @"Pass %@", selectorName);
+            }
+        }
+    }
+}
+
+@end
