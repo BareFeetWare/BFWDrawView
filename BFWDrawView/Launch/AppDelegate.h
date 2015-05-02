2ac83ec706e247fcd5d1c0ec61e76916c93b1aec
Tom Brodhurst-Hill <developer@barefeetware.com>
2015-05-03 00:52:22 +1000

    Restructured groups in project. Updated SampleStyleKit with animated Pac Man

---
+++ BFWDrawView/Launch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFWDrawView
+//
+//  Created by Tom Brodhurst-Hill on 1/02/2015.
+//  Copyright (c) 2015 BareFeetWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
