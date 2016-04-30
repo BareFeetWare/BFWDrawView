7fecf9c0c737c27a3e2391d9592a2cb7a1a20f4c
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-04-30 15:35:45 +1000

    DrawingView: Swift subclass of BFWDrawView, which it will eventually replace. Using DrawingView instead of BFWDrawView in storyboards in Swift projects seems to fix the unknown class warnings.

---
+++ BFWDrawView/Modules/Draw/View/DrawingView.swift
@@ -0,0 +1,15 @@
+//
+//  DrawingView.swift
+//  BFWDrawView
+//
+//  Created by Tom Brodhurst-Hill on 30/04/2016.
+//  Copyright © 2016 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+
+/// DrawingView is a Swift class that will eventually replace BFWDrawView
+@IBDesignable class DrawingView: BFWDrawView {
+
+
+}
