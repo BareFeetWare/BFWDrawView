0fe067f76d21440b678f7e06ce91b9611421f870
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-05-20 15:51:57 +1000

    AnimationView: Swift subclass of Objective C BFWAnimationView

---
+++ BFWDrawView/Modules/Draw/View/AnimationView.swift
@@ -0,0 +1,14 @@
+//
+//  AnimationView.swift
+//  BFWDrawView
+//
+//  Created by Tom Brodhurst-Hill on 20/05/2016.
+//  Copyright © 2016 BareFeetWare. All rights reserved.
+//
+
+import UIKit
+
+/// AnimationView is a Swift class that will eventually replace BFWAnimationView
+@IBDesignable class AnimationView: BFWAnimationView {
+
+}
