d54a88356439c2ef74229119ee2528675c210da0
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-06-09 23:02:26 +1000

    AnimationView: implemented curves for Linear (default), EaseInOut, EaseIn, EaseOut, EaseOutElastic

--- BFWDrawView/Modules/Draw/View/AnimationView.swift
+++ BFWDrawView/Modules/Draw/View/AnimationView.swift
@@ -13,35 +13,52 @@ import UIKit
 
     enum Curve: Int {
         
-        case linear = 0
-        case easeOutElastic = 1
+        case Linear = 0
+        case EaseInOut = 1
+        case EaseIn = 2
+        case EaseOut = 3
+        case EaseOutElastic = 4
         
         var function: (Double) -> Double {
             switch self {
-            case .linear: return { t in t }
-            case .easeOutElastic:
+            case .Linear: return { t in t }
+            case .EaseInOut:
+                return { t in
+                    let easeIn = Curve.EaseIn.function
+                    return t < 0.5 ? easeIn(t * 2.0) / 2.0 : 1.0 - easeIn((1.0 - t ) * 2) / 2
+                }
+            case .EaseIn: return { t in pow(t, 3.0) }
+            case .EaseOut: return { t in 1.0 - pow(1.0 - t, 3.0) }
+            case .EaseOutElastic:
                 return { t in
                     let p = 0.3
-                    return pow(2.0, -10 * t) * sin((t - p / 4) * (2 * M_PI) / p) + 1
+                    let max = 1.375
+                    let outside = pow(2.0, -10 * t) * sin((t - p / 4) * (2 * M_PI) / p) + 1
+                    return outside / max
                 }
             }
         }
     }
     
-    var curve: Curve = .linear
+    var curve: Curve = .Linear
     
     @IBInspectable var curve_: Int {
         get {
             return curve.rawValue
         }
         set {
-            curve = Curve(rawValue: newValue) ?? .linear
+            curve = Curve(rawValue: newValue) ?? .Linear
         }
     }
     
-    var animationBetweenStartAndEnd: Double {
-        let t = animation as Double
-        return curve.function(t)
+    // Overriding BFWAnimationView
+    
+    var animationBetweenStartAndEnd: CGFloat {
+        var curved = curve.function(animation)
+        if start != 0.0 || end != 0.0 {
+            curved = start + curved * (end - start)
+        }
+        return CGFloat(curved)
     }
     
 }
