9a350daa5684052c8215397e11e551f61ffd01cf
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-06-09 17:36:28 +1000

    AnimationView: curve: started implementing

--- BFWDrawView/Modules/Draw/View/AnimationView.swift
+++ BFWDrawView/Modules/Draw/View/AnimationView.swift
@@ -11,4 +11,37 @@ import UIKit
 /// AnimationView is a Swift class that will eventually replace BFWAnimationView
 @IBDesignable class AnimationView: BFWAnimationView {
 
+    enum Curve: Int {
+        
+        case linear = 0
+        case easeOutElastic = 1
+        
+        var function: (Double) -> Double {
+            switch self {
+            case .linear: return { t in t }
+            case .easeOutElastic:
+                return { t in
+                    let p = 0.3
+                    return pow(2.0, -10 * t) * sin((t - p / 4) * (2 * M_PI) / p) + 1
+                }
+            }
+        }
+    }
+    
+    var curve: Curve = .linear
+    
+    @IBInspectable var curve_: Int {
+        get {
+            return curve.rawValue
+        }
+        set {
+            curve = Curve(rawValue: newValue) ?? .linear
+        }
+    }
+    
+    var animationBetweenStartAndEnd: Double {
+        let t = animation as Double
+        return curve.function(t)
+    }
+    
 }
