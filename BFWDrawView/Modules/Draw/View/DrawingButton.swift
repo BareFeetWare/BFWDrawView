e7ccb7eb9503557bfe68d8a100682b80cf96544e
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-07-09 21:55:42 +1000

    DrawingButton: iconName and iconStyleKit

---
+++ BFWDrawView/Modules/Draw/View/DrawingButton.swift
@@ -0,0 +1,58 @@
+//
+//  DrawingButton.swift
+//  BFWDrawView
+//
+//  Created by Tom Brodhurst-Hill on 9/07/2016.
+//  Copyright (c) 2016 BareFeetWare. All rights reserved.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
+//
+
+import UIKit
+
+class DrawingButton: BFWDrawButton {
+
+    // MARK: - Variables
+
+    @IBInspectable var iconName: String? {
+        didSet {
+            setNeedsUpdateView()
+        }
+    }
+    
+    @IBInspectable var iconStyleKit: String? {
+        didSet {
+            setNeedsUpdateView()
+        }
+    }
+    
+    // MARK: - UpdateView
+    
+    private func setNeedsUpdateView() {
+        needsUpdateView = true
+        setNeedsLayout()
+    }
+    
+    private var needsUpdateView = true
+    
+    private func updateView() {
+        if let iconName = iconName, iconStyleKit = iconStyleKit {
+            makeIconDrawViewsFromStateNameDict([UIControlState.Normal.rawValue: iconName],
+                                               styleKit: iconStyleKit)
+        }
+    }
+    
+    private func updateViewIfNeeded() {
+        if needsUpdateView {
+            needsUpdateView = false
+            updateView()
+        }
+    }
+    
+    // MARK: - UIView
+    
+    override func layoutSubviews() {
+        updateViewIfNeeded()
+        super.layoutSubviews()
+    }
+
+}
