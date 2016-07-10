0e2d4c37b2908d51aeed151665e9fde7acaf0540
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-07-11 03:16:22 +1000

    Bridging-Header: Added BFWDrawButton.h

--- BFWDrawView/Launch/BFWDrawView-Bridging-Header.h
+++ BFWDrawView/Launch/BFWDrawView-Bridging-Header.h
@@ -5,6 +5,7 @@
 #import "AnimationCountViewController.h"
 #import "BFWAnimationView.h"
 #import "BFWDrawExport.h"
+#import "BFWDrawButton.h"
 #import "BFWDrawView.h"
 #import "BFWStyleKit.h"
 #import "BFWStyleKitDrawing.h"
