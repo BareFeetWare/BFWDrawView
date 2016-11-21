f9dab35ce580d14d6a3f0e8eebe81fc5f45af7ac
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-11-21 15:29:18 +1100

    SampleDrawingButton: updated makeBackgroundDrawViews syntax

--- BFWDrawView/Modules/Sample/View/SampleDrawingButton.swift
+++ BFWDrawView/Modules/Sample/View/SampleDrawingButton.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 @IBDesignable class SampleDrawingButton: DrawingButton {
-
+    
     override func commonInit() {
         super.commonInit()
-        makeBackgroundDrawViews(fromStateNameDict: [UIControlState.normal.rawValue: "Button",
-                                                    UIControlState.highlighted.rawValue: "Button Highlighted"],
+        makeBackgroundDrawViews(from: [UIControlState.normal.rawValue: "Button",
+                                       UIControlState.highlighted.rawValue: "Button Highlighted"],
                                 styleKit: "SampleStyleKit")
         setShadow(SampleStyleKit.buttonShadow(),
                   for:.normal)
         setShadow(SampleStyleKit.buttonShadowHighlighted(),
                   for:.highlighted)
     }
-
+    
     override var intrinsicContentSize: CGSize {
         return CGSize(width: 80.0, height: 44.0)
     }
-
+    
 }
