ed5cdf648c995df029157b1f1e2a68a5a0b03ba9
Tom Brodhurst-Hill <developer@barefeetware.com>
2016-11-21 15:11:19 +1100

    DrawingButton.swift: converted code from BFWDrawButton

--- BFWDrawView/Modules/Draw/View/DrawingButton.swift
+++ BFWDrawView/Modules/Draw/View/DrawingButton.swift
@@ -9,9 +9,15 @@
 
 import UIKit
 
-@IBDesignable class DrawingButton: BFWDrawButton {
+@IBDesignable class DrawingButton: UIButton {
 
     // MARK: - Variables
+
+    private var iconDrawViewForStateDict = [NSNumber: DrawingView]()
+    private var backgroundDrawViewForStateDict = [NSNumber: DrawingView]()
+    private var shadowForStateDict = [NSNumber: NSShadow]()
+    private var needsUpdateShadow = true
+    private var backgroundSize = CGSize.zero
 
     @IBInspectable var iconName: String? {
         didSet {
@@ -22,6 +28,123 @@ import UIKit
     @IBInspectable var iconStyleKit: String? {
         didSet {
             setNeedsUpdateView()
+        }
+    }
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        // Implement in subclasses if required and call super.
+    }
+    
+    // MARK: - Accessors for state
+    
+    func iconDrawView(for state: UIControlState) -> DrawingView? {
+        return iconDrawViewForStateDict[NSNumber(value: state.rawValue)]
+    }
+    
+    func backgroundDrawView(for state: UIControlState) -> DrawingView? {
+        return backgroundDrawViewForStateDict[NSNumber(value: state.rawValue)]
+    }
+    
+    func setIconDrawView(_ drawView: DrawingView, for state: UIControlState) {
+        iconDrawViewForStateDict.setValueOrRemoveNil(drawView, forKey: NSNumber(value: state.rawValue))
+        setImage(drawView.image, for: state)
+    }
+    
+    func setBackgroundDrawView(_ drawView: DrawingView, for state: UIControlState) {
+        backgroundDrawViewForStateDict.setValueOrRemoveNil(drawView.canDraw ? drawView : nil, forKey: NSNumber(value: state.rawValue))
+        setNeedsUpdateBackgrounds()
+    }
+    
+    func shadow(for state: UIControlState) -> NSShadow? {
+        return shadowForStateDict[NSNumber(value: state.rawValue)]
+    }
+    
+    func setShadow(_ shadow: NSShadow, for state: UIControlState) {
+        shadowForStateDict.setValueOrRemoveNil(shadow, forKey: NSNumber(value: state.rawValue))
+        setNeedsUpdateShadow()
+    }
+
+    func setNeedsUpdateShadow() {
+        needsUpdateShadow = true
+        setNeedsDisplay()
+    }
+    
+    func setNeedsUpdateBackgrounds() {
+        backgroundSize = CGSize.zero
+        setNeedsLayout()
+    }
+    
+    var needsUpdateBackgrounds: Bool {
+        return backgroundSize != bounds.size
+    }
+    
+    // MARK: - Updates
+    
+    func updateBackgrounds() {
+        for state: UIControlState in [.normal, .disabled, .selected, .highlighted] {
+            if let background = backgroundDrawViewForStateDict[NSNumber(value: state.rawValue)] {
+                background.frame = bounds
+                setBackgroundImage(background.image, for: state)
+            }
+        }
+    }
+    
+    func updateBackgroundsIfNeeded() {
+        if needsUpdateBackgrounds {
+            backgroundSize = bounds.size
+            updateBackgrounds()
+        }
+    }
+    
+    func updateShadowIfNeeded() {
+        if needsUpdateShadow {
+            needsUpdateShadow = false
+            let shadow = self.shadow(for: state) ?? self.shadow(for: .normal)
+            apply(shadow: shadow)
+        }
+    }
+    
+    // MARK: - UIButton
+    
+    override var isHighlighted: Bool {
+        get {
+            return super.isHighlighted
+        }
+        set {
+            super.isHighlighted = newValue
+            setNeedsUpdateShadow()
+        }
+    }
+    
+    override var isSelected: Bool {
+        get {
+            return super.isSelected
+        }
+        set {
+            super.isSelected = newValue
+            setNeedsUpdateShadow()
+        }
+    }
+    
+    override var isEnabled: Bool {
+        get {
+            return super.isEnabled
+        }
+        set {
+            super.isEnabled = newValue
+            setNeedsUpdateShadow()
         }
     }
     
@@ -42,13 +165,40 @@ import UIKit
             } else {
                 frame = drawing.intrinsicFrame
             }
-            let icon = BFWDrawView(frame: frame)
+            let icon = DrawingView(frame: frame)
             icon.drawing = drawing
             icon.tintColor = tintColor
             icon.contentMode = .redraw
             setIconDrawView(icon, for: state)
         }
     }
+
+    
+    func makeIconDrawViews(from stateNameDict: [NSNumber: String], styleKit: String) {
+        iconDrawViewForStateDict.removeAll()
+        for (stateNumber, drawingName) in stateNameDict {
+            if let drawing = BFWStyleKit.drawing(forStyleKitName: styleKit, drawingName: drawingName) {
+                let icon = DrawingView(frame: drawing.intrinsicFrame)
+                icon.drawing = drawing
+                icon.tintColor = tintColor
+                icon.contentMode = .redraw
+                setIconDrawView(icon, for: UIControlState(rawValue: stateNumber.uintValue))
+            }
+        }
+    }
+    
+    func makeBackgroundDrawViews(from stateNameDict: [NSNumber: String], styleKit: String) {
+        backgroundDrawViewForStateDict.removeAll()
+        for (stateNumber, drawingName) in stateNameDict {
+            let background = DrawingView(frame: self.bounds)
+            background.name = drawingName
+            background.styleKit = styleKit
+            background.contentMode = .redraw
+            setBackgroundDrawView(background,
+                for: UIControlState(rawValue: stateNumber.uintValue))
+        }
+    }
+
 
     // MARK: - UpdateView
     
@@ -63,7 +213,7 @@ import UIKit
         if let iconName = iconName,
             let iconStyleKit = iconStyleKit
         {
-            makeIconDrawViews(fromStateNameDict: [UIControlState.normal.rawValue: iconName],
+            makeIconDrawViews(from: [NSNumber(value: UIControlState.normal.rawValue): iconName],
                               styleKit: iconStyleKit)
         }
     }
@@ -78,8 +228,40 @@ import UIKit
     // MARK: - UIView
     
     override func layoutSubviews() {
+        updateBackgroundsIfNeeded()
         updateViewIfNeeded()
         super.layoutSubviews()
     }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        updateShadowIfNeeded()
+    }
+    
+}
 
+fileprivate extension Dictionary {
+    
+    mutating func setValueOrRemoveNil(_ valueOrNil: Value?, forKey key: Key) {
+        if let value = valueOrNil {
+            self[key] = value
+        } else {
+            self.removeValue(forKey: key)
+        }
+    }
+    
+}
+
+fileprivate extension UIView {
+    
+    func apply(shadow: NSShadow?) {
+        if let shadowColor = shadow?.shadowColor as? UIColor {
+            layer.shadowColor = shadowColor.cgColor
+        }
+        layer.shadowRadius = shadow?.shadowBlurRadius ?? 0.0
+        layer.shadowOffset = shadow?.shadowOffset ?? CGSize.zero
+        layer.shadowOpacity = shadow != nil ? 1.0 : 0.0
+        layer.masksToBounds = shadow == nil
+    }
+    
 }
