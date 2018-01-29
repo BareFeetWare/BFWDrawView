d72bb5c1af7031e812fa3c72dc3dce8f799b7705
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-01-29 23:45:43 +1100

    DrawNibTableViewCell

---
+++ BFWDrawNibView/Modules/DrawNibView/View/DrawNibTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  DrawNibTableViewCell.swift
+//  BFWDrawNibView
+//
+//  Created by Tom Brodhurst-Hill on 29/1/18.
+//  Copyright © 2018 BareFeetWare. All rights reserved.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
+//
+
+import UIKit
+import BFWControls
+import BFWDrawView
+
+open class DrawNibTableViewCell: NibTableViewCell {
+
+    // MARK - Variables
+    
+    @IBInspectable open var iconName: String? {
+        get {
+            return (cellView as? DrawNibCellView)?.iconDrawView?.name
+        }
+        set {
+            (cellView as? DrawNibCellView)?.iconDrawView?.name = newValue
+        }
+    }
+    
+    @IBInspectable open var styleKit: String? {
+        get {
+            return (cellView as? DrawNibCellView)?.iconDrawView?.styleKit
+        }
+        set {
+            (cellView as? DrawNibCellView)?.iconDrawView?.styleKit = newValue
+        }
+    }
+
+}
