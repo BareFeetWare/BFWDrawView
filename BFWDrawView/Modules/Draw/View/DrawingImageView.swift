bdb47174307b10b83d656d7107f28cd9abe79d94
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-05-05 23:08:17 +1000

    DrawingImageView

---
+++ BFWDrawView/Modules/Draw/View/DrawingImageView.swift
@@ -0,0 +1,83 @@
+//
+//  DrawingImageView.swift
+//  BFWDrawView
+//
+//  Created by Tom Brodhurst-Hill on 2/5/18.
+//  Copyright © 2018 BareFeetWare. All rights reserved.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
+//
+
+import UIKit
+
+open class DrawingImageView: UIImageView {
+    
+    // MARK: - Variables
+    
+    open var drawing: Drawing? { didSet { setNeedsUpdateImage() }}
+    @IBInspectable open var name: String? { didSet { updateDrawing() }}
+    @IBInspectable open var styleKit: String? { didSet { updateDrawing() }}
+    
+    // MARK: - Update drawing
+    
+    private func updateDrawing() {
+        // TODO: Call this only once for each stylekit and drawing name pair change.
+        guard let name = name,
+            let styleKitName = styleKit
+            else { return }
+        drawing = StyleKit.drawing(forStyleKitName: styleKitName,
+                                   drawingName: name)
+    }
+    
+    // MARK: - Update image
+    
+    private var needsUpdateImage = false
+    
+    private func setNeedsUpdateImage() {
+        needsUpdateImage = true
+        setNeedsLayout()
+    }
+    
+    private func updateImageIfNeeded() {
+        if needsUpdateImage {
+            needsUpdateImage = false
+            updateImage()
+        }
+    }
+    
+    private func updateImage() {
+        let drawingView = DrawingView(frame: bounds)
+        drawingView.drawing = drawing
+        drawingView.contentMode = contentMode
+        drawingView.tintColor = tintColor
+        image = drawingView.image
+    }
+    
+    // MARK: - UIView
+    
+    open override func tintColorDidChange() {
+        super.tintColorDidChange()
+        setNeedsUpdateImage()
+    }
+    
+    open override var frame: CGRect {
+        didSet {
+            if oldValue.size != frame.size {
+                setNeedsUpdateImage()
+            }
+        }
+    }
+    
+    open override var contentMode: UIViewContentMode {
+        didSet {
+            if oldValue != contentMode {
+                setNeedsUpdateImage()
+            }
+        }
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        updateImageIfNeeded()
+    }
+    
+}
