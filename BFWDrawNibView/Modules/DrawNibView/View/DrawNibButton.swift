1c07fb523d88648aba91e3335cb58cc6e68a762d
Tom Brodhurst-Hill <developer@barefeetware.com>
2018-11-25 23:48:10 +1100

    DrawNibButton: subclass of NibButton. DrawNibContainerButton: deprecated.

---
+++ BFWDrawNibView/Modules/DrawNibView/View/DrawNibButton.swift
@@ -0,0 +1,38 @@
+//
+//  DrawNibButton.swift
+//  BFWDrawNibView
+//
+//  Created by Tom Brodhurst-Hill on 25/11/18.
+//  Copyright © 2018 BareFeetWare. All rights reserved.
+//  Free to use at your own risk, with acknowledgement to BareFeetWare.
+//
+
+import UIKit
+import BFWControls
+import BFWDrawView
+
+open class DrawNibButton: NibButton {
+    
+    open var drawingImageView: DrawingImageView? {
+        return imageView as? DrawingImageView
+    }
+    
+    @IBInspectable open var drawingStyleKit: String? {
+        get {
+            return drawingImageView?.styleKit
+        }
+        set {
+            drawingImageView?.styleKit = newValue
+        }
+    }
+    
+    @IBInspectable open var drawingName: String? {
+        get {
+            return drawingImageView?.name
+        }
+        set {
+            drawingImageView?.name = newValue
+        }
+    }
+    
+}
